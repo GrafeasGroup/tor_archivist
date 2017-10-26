2bca5ecdb8eaa84d8fc69f4512c2f2527a57d162
David Alexander <opensource@thelonelyghost.com>
2017-10-25 22:20:45 -0400

    Fixes test since command -v doesn't exist on TravisCI

--- test/test_has_command.py
+++ test/test_has_command.py
@@ -1,12 +1,16 @@
-import os
 from io import StringIO
+import re
 
+import pytest
 import sh
 
 
 def test_generated_command_exists():
     out = StringIO()
-    sh.command('-v', 'tor-archivist', _out=out)
-    out.seek(0)
-    cmd = out.read().strip()
-    assert os.path.basename(cmd) == 'tor-archivist'
+
+    with pytest.raises(sh.ErrorReturnCode):
+        sh.tor_archivist(_out=out, _err=out)
+
+    lines = out.getvalue().strip().splitlines()
+    pattern = re.compile('bugsnag.* No API Key', re.IGNORECASE)
+    assert any(pattern.search(line) for line in lines)
