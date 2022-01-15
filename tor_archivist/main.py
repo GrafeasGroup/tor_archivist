c916e07c052d53d7246244301c861a8853189cdd
Tim Jentzsch <tim-jentzsch@gmx.de>
2022-01-16 00:38:39 +0100

    Allow specification of update delay via env variables

--- tor_archivist/main.py
+++ tor_archivist/main.py
@@ -12,7 +12,6 @@ from tor_archivist.core.config import Config
 from tor_archivist.core.config import config
 from tor_archivist.core.helpers import run_until_dead, get_id_from_url
 from tor_archivist.core.initialize import build_bot
-from tor_archivist.core.strings import reddit_url
 
 dotenv.load_dotenv()
 
@@ -21,16 +20,15 @@ CLEAR_THE_QUEUE_MODE = bool(os.getenv("CLEAR_THE_QUEUE", ""))
 NOOP_MODE = bool(os.getenv("NOOP_MODE", ""))
 DEBUG_MODE = bool(os.getenv("DEBUG_MODE", ""))
 
+UPDATE_DELAY_SEC = int(os.getenv("UPDATE_DELAY_SEC", 60 * 5))
+
 DISABLE_COMPLETED_ARCHIVING = bool(os.getenv("DISABLE_COMPLETED_ARCHIVING", False))
 DISABLE_EXPIRED_ARCHIVING = bool(os.getenv("DISABLE_EXPIRED_ARCHIVING", False))
 DISABLE_POST_REMOVAL_TRACKING = bool(os.getenv("DISABLE_POST_REMOVAL_TRACKING", False))
 
 # TODO: Remove the lines below with hardcoded versions.
 __VERSION__ = "1.0.0"
-
 ##############################
-
-thirty_minutes = 1800  # seconds
 
 
 def parse_arguments():
@@ -129,7 +127,7 @@ def archive_completed_posts(cfg: Config) -> None:
 def track_post_removal(cfg: Config) -> None:
     """Process the mod log and sync post removals to Blossom."""
     tor = cfg.r.subreddit("TranscribersOfReddit")
-    for log in tor.mod.log(action="removelink", limit=20):
+    for log in tor.mod.log(action="removelink", limit=100):
         mod = log.mod
         tor_url = "https://reddit.com" + log.target_permalink
         create_time = datetime.datetime.fromtimestamp(log.created_utc)
@@ -180,7 +178,7 @@ def run(cfg: Config) -> None:
     if CLEAR_THE_QUEUE_MODE:
         logging.info("Clear the Queue Mode is engaged!")
     else:
-        cfg.sleep_until = time.time() + thirty_minutes
+        cfg.sleep_until = time.time() + UPDATE_DELAY_SEC
 
     logging.info("Starting archiving of old posts...")
 
